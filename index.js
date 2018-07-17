a4c7b4d6e1825dd0727bbc22884c0481c9f52e55
Pavel Nekrasov <pavel@resultdevelopment.com>
2018-07-17 14:16:29 +0200

    http api

--- index.js
+++ index.js
@@ -1,12 +1,72 @@
 const assert = require('assert'),
-	OrientDB = require('orientjs');
+	requestPromise = require('request-promise')
+//OrientDB = require('orientjs');
+
+const connect = Symbol("connect")
 
 module.exports = function () {
 	assert(!this.orientDB, "field exists")
 
-	const client = OrientDB(this.config.get("orientdb"));
+	const shared = this;
 
-	this.orientDB = client.use(this.config.get("orientdb"));
+	class OrientDB {
+		constructor(config) {
+			this.config = config;
+		}
+
+		/*[connect](force) {
+			if (!force && this.auth){
+				return Promise.resolve();
+			}
+			return requestPromise({
+				method: 'GET',
+				uri: `http://${this.config.host}:${this.config.port}/connect/${this.config.name}`,
+				headers: {
+					"Authorization": `Basic ${Buffer.from(this.config.username + ':' + this.config.password).toString('base64')}`
+				},
+				resolveWithFullResponse: true
+			}).then((res) => {
+				return requestPromise({
+					method: 'POST',
+					uri: `http://${this.config.host}:${this.config.port}/server/configuration.command.timeout/${this.config.timeout * 1000}`,
+					headers: {
+						"Authorization": `Basic ${Buffer.from(this.config.username + ':' + this.config.password).toString('base64')}`
+					},
+					resolveWithFullResponse: true
+				}).then(() => {
+					this.auth = res.headers["set-cookie"][0];
+				})
+			}).catch((err) => {
+				this.auth = null;
+			})
+		}*/
+
+		query(command, parameters) {
+			return new Promise((resolve, reject)=>{
+				requestPromise({
+					method: 'POST',
+					uri: `http://${this.config.host}:${this.config.port}/command/${this.config.name}/sql/25`,
+					headers: {
+						"Authorization": `Basic ${Buffer.from(this.config.username + ':' + this.config.password).toString('base64')}`
+					},
+					json: true,
+					body: {
+						command,
+						parameters
+					}
+				}).then((res) => {
+					resolve(res.result)
+				}).catch((err) => {
+					reject(err)
+				})
+			})
+		}
+	}
+
+	this.orientDB = new OrientDB(this.config.get("orientdb"));
+	//const client = OrientDB(this.config.get("orientdb"));
+
+	//this.orientDB = client.use(this.config.get("orientdb"));
 
 	return Promise.resolve();
 }
